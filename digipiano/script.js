b04fe78f5e263afa40c1813c1699af9e13228622
Simran Sharma <103310345+SimranS22@users.noreply.github.com>
2024-05-12 16:07:32 +0530

    Delete digipiano directory

--- digipiano/script.js
+++
@@ -1,6 +0,0 @@
-function playSound(note){
-    var audio = new Audio("sounds/"+ note + ".mp3");
-    audio.play();
-}
-
-// for the above function to work, you need to have mp3 files of all the notes (A.mp3, CC.mp3, etc.)
